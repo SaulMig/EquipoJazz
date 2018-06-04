6e64b56d1b148508571e9067fa4731b0dcd810c6
FelipeEnriqueGG <isc_garcia.f@tesvb.edu.mx>
2018-06-04 15:31:01 -0500

    footer

--- Index.php
+++ Index.php
@@ -10,6 +10,4 @@
     if(!isset($_SERVER["HTTP_X_REQUESTED_WITH"]))
     Views\Template::header();
     AppData\Config\Enrutador::run(new AppData\Config\Request());
-    if(!isset($_SERVER["HTTP_X_REQUESTED_WITH"]))
-    Views\Template::footer();
 ?>
