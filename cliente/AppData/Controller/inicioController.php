f07492a1412359df03b6eca1fdd62288da8a628d
Jazminestanislao <isc_estanislao.j@tesvb.edu.mx>
2018-06-09 13:34:21 -0500

    Template cliente

---
+++ cliente/AppData/Controller/inicioController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JAZMIN
+ * Date: 10/05/2018
+ * Time: 01:12 PM
+ */
+
+namespace AppData\Controller;
+
+
+class inicioController
+{
+    public function index()
+    {
+       // echo "Hola desde mi controlador";
+    }
+    public function crear()
+    {
+
+    }
+    public function hola()
+    {
+
+    }
+}
