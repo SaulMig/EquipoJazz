ed0009ab84b69a6807e867f88ad346e4bc2b13f6
YareMorales22 <isc_morales.y@tesvb.edu.mx>
2018-06-25 13:49:27 -0500

    PDF

---
+++ Views/personas/print_pdf.php
@@ -0,0 +1,30 @@
+<?php
+
+$pdf = new \AppData\Config\libs\fpdf\FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(190,10,'Lista de pacientes',0,0,'C');
+$pdf->ln();
+$pdf->ln();
+$pdf->SetFillColor(0,220,255);
+$pdf->SetTextColor(255,255,255);
+$pdf->SetFont('Arial','B',10);
+$pdf->SetX(25);
+$pdf->Cell(40,10,utf8_decode(''),1,0,'C','true');
+$pdf->Cell(40,10,utf8_decode('Descripcion'),1,0,'C', 'true');
+$pdf->Cell(40,10,utf8_decode('Tipo Habitacion'),1,0,'C', 'true');
+$pdf->Cell(40,10,utf8_decode('Estado Habitacion'),1,0,'C', 'true');
+$pdf->SetFillColor(255,255,255);
+$pdf->SetTextColor(0,0,0);
+$pdf->SetFont('Arial','B',8);
+while($row=mysqli_fetch_row($datos))
+{
+    $pdf->ln();
+    $pdf->SetX(25);
+    $pdf->Cell(40,10,utf8_decode($row[1]),1,0,'C');
+    $pdf->Cell(40,10,utf8_decode($row[2]),1,0,'C');
+    $pdf->Cell(40,10,utf8_decode($row[3]),1,0,'C');
+    $pdf->Cell(40,10,utf8_decode($row[4]),1,0,'C');
+}
+$pdf->Output();
+
